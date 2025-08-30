2919dc8751605d1a31170cb776d567d531da1792
Manideep Guthi <manideep.guthi@gmail.com>
2025-08-30 11:39:11 +0530

    Updated port for Render deployment

--- index.js
+++ index.js
@@ -306,6 +306,7 @@ app.get("/logout", (req, res) => {
 // );
 
 
-app.listen(5000, '0.0.0.0', () => {
-  console.log("Server running at http://192.168.1.11:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
